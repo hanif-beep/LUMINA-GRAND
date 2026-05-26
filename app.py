92601b91b233d9467d8905b47320320b52b53ddf
hanif-beep <fajrinurhanif77@gmail.com>
2026-05-26 12:49:42 +0700

    fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from flask import request, redirect
 
 app = Flask(__name__)
 
@@ -22,6 +23,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), default='user')
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(255))
+    status = db.Column(db.String(20), default='available')
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -64,6 +74,37 @@ def login():
             return redirect('/')
 
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    rooms = Room.query.all()
+    return render_template('Dashboard.html', rooms=rooms)
+
+@app.route('/add_room', methods=['GET', 'POST'])
+def add_room():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        image = request.form['image']
+        status = request.form['status']
+
+        new_room = Room(
+            name=name,
+            price=price,
+            description=description,
+            image=image,
+            status=status
+        )
+
+        db.session.add(new_room)
+        db.session.commit()
+
+        return redirect('/dashboard')
+
+    return render_template('add_room.html')
 
 # MEMBUAT DATABASE
 with app.app_context():
